de3fa42ad4ef82e3463506786953b9bfe0ce53b4
tomazcx <tomazcx06@gmail.com>
2022-10-26 22:10:42 -0300

    Added unique key

--- components/Home/ProductsHome.tsx
+++ components/Home/ProductsHome.tsx
@@ -6,9 +6,15 @@ interface ProductsInterface {
 }
 
 export const ProductsHome = (props: ProductsInterface) => {
+    
+    let cont = 0
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 gap-24 px-12 animate-show-glasses">
-            {props.img.map(img => <CardProduct img={img} />)}
+            {props.img.map((img) => {
+                cont++
+                return <CardProduct key={cont} img={img} />
+            })}
         </div>
     )
 
