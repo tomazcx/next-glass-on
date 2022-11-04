d635529daeb5b3c025c3e7b2ee0bf86a7aaa8dd1
tomazcx <tomazcx06@gmail.com>
2022-11-03 21:12:58 -0300

    Added button to search modal

--- components/Modals/HeaderSearch.tsx
+++ components/Modals/HeaderSearch.tsx
@@ -19,7 +19,9 @@ export const HeaderSearch = ({close} : ModalInterface) => {
     return (
         <div className='bg-white shadow-2xl px-4 md:px-28 py-4 flex items-center justify-between'>
             <form onSubmit={(e) => handleSubmit(e)} className="flex gap-4 items-center">
-                <MagnifyingGlass size={24} color="#000" className='cursor-pointer' />
+                <button type="submit">
+                    <MagnifyingGlass size={24} color="#000" className='cursor-pointer' />
+                </button>
                 <input onChange={(e) => setText(e.target.value)} placeholder='Pesquisar' type="text" className="text-black bg-transparent border-none outline-none" />
             </form>
             <X size={24} color="#0d0d0d" onClick={() => close(false)} className='cursor-pointer' />
