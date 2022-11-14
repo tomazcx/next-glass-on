8a7755f65dd5aed8d79f7afb39593d9c9cb4d8ab
tomazcx <tomazcx06@gmail.com>
2022-11-14 19:17:19 -0300

    Removed required for state query

--- graphql/mutations/update/updateClient.ts
+++ graphql/mutations/update/updateClient.ts
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export const UPDATE_CLIENT = gql`
-    mutation UpdateClient($id:ID!,$birthDate: String, $cep: String, $city: String, $complement: String, $cpf:String, $district: String, $houseNum: String, $email: String, $name: String, $street: String, $building:String, $state: String!) {
+    mutation UpdateClient($id:ID!,$birthDate: String, $cep: String, $city: String, $complement: String, $cpf:String, $district: String, $houseNum: String, $email: String, $name: String, $street: String, $building:String, $state: String) {
         updateClient(
             data: {birthDate: $birthDate, cep: $cep, city: $city, complement: $complement, cpf: $cpf, district: $district, houseNum: $houseNum, email: $email, name: $name, street: $street, building: $building, state: $state}
         where: {id: $id}
